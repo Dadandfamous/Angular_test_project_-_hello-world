e1b89df49af6fcc23286ee7be0a6ec0a634886a3
Martin <stuurmartinpost@gmail.com>
2019-06-30 14:08:30 +0200

    transferred list of courses to separate component

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -16,8 +16,9 @@ import { Component } from '@angular/core'
 
 export class CoursesComponent {
     title = "List of courses"
-    courses = ["course1", "course2", "course3"]
+    courses;
 
+    // logic of http service
 
     // getTitle() {
     //   return this.title
